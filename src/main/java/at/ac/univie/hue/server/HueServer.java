c914fcaf666d33ea0fd406f11038e7cf0f826943
Alexander Gelb <alexander.gelb@gmx.at>
2016-10-03 14:21:11 +0200

    added update hue jersey webservice

---
+++ src/main/java/at/ac/univie/hue/server/HueServer.java
@@ -0,0 +1,34 @@
+package at.ac.univie.hue.server;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.Invocation;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.json.JSONObject;
+
+@Path("/service")
+public class HueServer {
+    
+    @PUT
+    @Path("/update-light")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateLight(String json) {
+        if (json == null || json.isEmpty()) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        
+        Client client = ClientBuilder.newClient();
+        WebTarget webTarget = client.target("http://10.0.0.13/").path("api/6dd2506f56ef41c9119969894be4468d/lights/1/state");
+        Invocation.Builder invocationBuilder = webTarget.request(MediaType.APPLICATION_JSON);
+        Response response = invocationBuilder.put(Entity.entity(json, MediaType.APPLICATION_JSON));
+        
+        return response;
+    }
+    
+}
